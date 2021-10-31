582c02661f8c297c085a9619a06519a296bfad5c
wigryz <tomasz.ligeza3@gmail.com>
2021-10-31 14:50:51 +0100

    First version

---
+++ src/main/java/com/example/gostocksum/GoStockSumApplication.java
@@ -0,0 +1,13 @@
+package com.example.gostocksum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoStockSumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoStockSumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gostocksum/GoStockSumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gostocksum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoStockSumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
